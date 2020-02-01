2f67b7e288d3d76b49653af341577cd8c0e5a530
Wujie Wang <wwj@mit.edu>
2020-01-31 19:40:01 -0500

    plot 2d potentials

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 
 from ase import io 
 from ase import Atoms 
+import numpy as np 
+import torch
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams.update({'font.size': 15})
+matplotlib.rc('lines', linewidth=4, color='g')
+matplotlib.rcParams['axes.linewidth'] = 3.0
 
 def dump_mov(movie, ref_atoms, fname="./LJ.pdb"):
 	atoms_list = []
@@ -11,3 +19,35 @@ def dump_mov(movie, ref_atoms, fname="./LJ.pdb"):
 	    atoms_list.append(sys) 
 
 	io.write(fname, atoms_list)
+
+
+def plot_lesp(model, traj=None, res=50, start=[3.5, 0.8], end=[0.8, 3.5], fname=None):
+    xlist = np.linspace(0.5, 5.0, res)
+    ylist = np.linspace(0.5, 5.0, res)
+    X, Y = np.meshgrid(xlist, ylist)
+
+    data = torch.Tensor(np.concatenate((X[:,:, None],Y[:,:,None]), axis=2).reshape(-1,2))
+    E = model(data).detach().cpu().numpy().reshape(res, res)
+    
+    plt.figure(figsize=(7,7))
+    cp = plt.contourf(X, Y, E, 40, cmap='GnBu', alpha=0.4)
+    plt.colorbar(cp)
+    
+    if traj is not None:
+        traj = traj.detach().cpu().numpy()
+        colors = cm.rainbow(np.linspace(0, 1, traj.shape[0]))
+
+        for i,c in enumerate(colors):
+
+            plt.scatter(traj[i, 2], traj[i, 3], color=c, s=1)
+        
+    plt.scatter(start[0], start[1], c='red')
+    plt.scatter(end[0], end[1], c='red')
+    
+    plt.xlim((0.5, 5.0))
+    plt.ylim((0.5, 5.0))
+    
+    if fname is not None:
+        plt.savefig(fname)
+    
+    plt.show()
