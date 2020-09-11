9675114e412bc0ea322a8bf5a756afc433c60f93
Wujie Wang <wwj@mit.edu>
2020-09-10 23:17:35 -0400

    clean imports

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,5 +1,4 @@
 from sigopt import Connection
-from sigopt.examples import franke_function
 
 import os
 import numpy as np
@@ -16,9 +15,8 @@ import torch
 from torch.optim import Adam
 from torchmd.md import NHCHAIN_ODE
 from torchmd.observable import rdf
-from torchmd.potentials import PairPot, MLP, LennardJones, ExcludedVolume, Buck, LennardJones69
+from torchmd.potentials import ExcludedVolume, 
 from torchmd.utils import *
-from nff.io.ase import AtomsBatch
 
 from ase import Atoms
 from nff.utils.scatter import compute_grad
@@ -29,7 +27,6 @@ from ase.neighborlist import neighbor_list
 from ase.lattice.cubic import FaceCenteredCubic, SimpleCubic
 from ase.geometry import wrap_positions
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-from ase import units
 
 from datetime import datetime
 
@@ -39,7 +36,6 @@ from torchmd.sovlers import odeint_adjoint, odeint
 from nff.utils.scatter import compute_grad
 from nff.utils import batch_to
 from torch.nn import ModuleDict
-from nff.train import Trainer, get_trainer, get_model
 
 matplotlib.rcParams.update({'font.size': 25})
 matplotlib.rc('lines', linewidth=3, color='g')
