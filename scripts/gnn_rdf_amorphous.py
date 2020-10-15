095e591f59be9d49455361bc5fbf31145a189753
Wujie Wang <wwj@mit.edu>
2020-10-15 17:35:50 -0400

    extend range of Si rdf

--- scripts/gnn_rdf_amorphous.py
+++ scripts/gnn_rdf_amorphous.py
@@ -10,6 +10,7 @@ from torchmd.md import Simulations
 from torchmd.observable import angle_distribution
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase import units
+
 import math 
 
 width_dict = {'tiny': 64,
@@ -31,6 +32,8 @@ angle_data_dict = {
         }
 }
 
+from ase.lattice.cubic import FaceCenteredCubic, Diamond
+
 rdf_data_dict = {
     'Si_2.293_100K': { 'fn': '../data/a-Si/100K_2.293.csv' ,
                        'rho': 2.293,
@@ -38,7 +41,19 @@ rdf_data_dict = {
                         'start': 1.8, 
                         'end': 7.9,
                         'element': "H",
-                        'mass': 28.0855
+                        'mass': 28.0855,
+                        "N_unitcell": 8,
+                        "cell": Diamond
+                        },
+    'Si_2.287_83K': { 'fn': '../data/a-Si/83K_2.287_exp.csv' ,
+                       'rho': 2.287,
+                        'T': 83.0, 
+                        'start': 1.8, 
+                        'end': 10.0,
+                        'element': "H",
+                        'mass': 28.0855,
+                        "N_unitcell": 8,
+                        "cell": Diamond
                         },
 
     'H20_0.997_298K': { 'fn': "../data/water_exp/water_exp_pccp.csv",
@@ -47,7 +62,21 @@ rdf_data_dict = {
                         'start': 1.8, 
                         'end': 7.5,
                         'element': "H" ,
-                        'mass': 18.01528
+                        'mass': 18.01528,
+                        "N_unitcell": 8,
+                        "cell": Diamond #FaceCenteredCubic
+                        },
+
+    'H20_0.978_342K': { 'fn': "../data/water_exp/water_exp_skinner_342K_0.978.csv",
+                       'rho': 0.978,
+                        'T': 342.0, 
+                        'start': 1.8, 
+                        'end': 7.5,
+                        'element': "H" ,
+                        'mass': 18.01528,
+                        "N_unitcell": 8,
+                        "cell": Diamond,
+                        "ref": "https://doi.org/10.1063/1.4902412"
                         },
 
     'Argon_1.417_298k': { 'fn': "../data/argon_exp/argon_exp.csv",
@@ -55,22 +84,23 @@ rdf_data_dict = {
                         'T': 298.0, 
                         'start': 2.0, 
                         'end': 9.0,
-                        'element': "H" ,
-                        'mass': 39.948
+                        'element': "H",
+                        'mass': 39.948,
+                        "N_unitcell": 4,
+                        "cell": FaceCenteredCubic
                         }
 }
 
 
-def get_fcc_unit_len(rho, mass):
+def get_unit_len(rho, mass, N_unitcell):
     
-    Na = 6.02214086 * 10**23 
+    Na = 6.02214086 * 10**23 # avogadro number 
 
     N = (rho * 10**6 / mass) * Na  # number of molecules in 1m^3 of water 
 
     rho = N / (10 ** 30) # number density in 1 A^3
-
-    N_mol = 4 # FCC unit cell has 4 atoms   
-    L = (N_mol / rho) ** (1/3)
+ 
+    L = (N_unitcell / rho) ** (1/3)
     
     return L 
 
@@ -112,8 +142,8 @@ def JS_rdf(g_obs, g):
 
     return loss_js
 
-def plot_rdfs(bins, target_g, simulated_g, fname, path):
-    plt.title("epoch {}".format(fname))
+def plot_rdfs(bins, target_g, simulated_g, fname, path, pname=None):
+    plt.title("epoch {}".format(pname))
     plt.plot(bins, simulated_g.detach().cpu().numpy() , linewidth=4, alpha=0.6, label='sim.' )
     plt.plot(bins, target_g.detach().cpu().numpy(), linewidth=2,linestyle='--', c='black', label='exp.')
     plt.xlabel("$\AA$")
@@ -122,17 +152,75 @@ def plot_rdfs(bins, target_g, simulated_g, fname, path):
     plt.show()
     plt.close()
 
+
+def get_system(data_str, device, size):
+
+    rho = rdf_data_dict[data_str]['rho']
+    mass = rdf_data_dict[data_str]['mass']
+    T = rdf_data_dict[data_str]['T']
+
+    # initialize states with ASE 
+    cell_module = rdf_data_dict[data_str]['cell']
+    N_unitcell = rdf_data_dict[data_str]['N_unitcell']
+
+    L = get_unit_len(rho, mass, N_unitcell)
+
+    print("lattice param:", L)
+
+    atoms = cell_module(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                              symbol='H',
+                              size=(size, size, size),
+                              latticeconstant= L,
+                              pbc=True)
+    system = System(atoms, device=device)
+    system.set_temperature(T)
+
+    return system 
+
+def get_sim(system, model, data_str):
+
+    T = rdf_data_dict[data_str]['T']
+
+    diffeq = NoseHooverChain(model, 
+            system,
+            Q=50.0, 
+            T=T * units.kB,
+            num_chains=5, 
+            adjoint=True).to(system.device)
+
+    # define simulator with 
+    sim = Simulations(system, diffeq)
+
+    return sim
+
+def get_observer(system, data_str, nbins):
+
+    data_path = rdf_data_dict[data_str]['fn']
+    data = np.loadtxt(data_path, delimiter=',')
+
+    # define the equation of motion to propagate 
+    start = rdf_data_dict[data_str]['start']
+    end = rdf_data_dict[data_str]['end']
+
+    xnew = np.linspace(start, end, nbins)
+        # initialize observable function 
+    obs = rdf(system, nbins, (start, end) )
+
+    # get experimental rdf 
+    count_obs, g_obs = get_exp_rdf(data, nbins, (start, end), obs)
+
+    return xnew, g_obs, obs
+
+
 def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
     # parse params 
-    #data = sys_params['data']
     size = sys_params['size']
-    #end = sys_params['end']
     tmax = sys_params['tmax']
     dt = sys_params['dt']
     n_epochs = sys_params['n_epochs'] 
     n_sim = sys_params['n_sim'] 
     cutoff = assignments['cutoff']
-    frameskip = math.ceil(assignments['frameskip_ratio'] * assignments['opt_freq'])
+    frameskip =  2 
 
     print('frames skip: ', frameskip)
 
@@ -161,85 +249,43 @@ def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
 
 
     # get cell parameter and data 
-    data_path = rdf_data_dict[sys_params['data']]['fn']
-    data = np.loadtxt(data_path, delimiter=',')
-    rho = rdf_data_dict[sys_params['data']]['rho']
-    mass = rdf_data_dict[sys_params['data']]['mass']
-    L = get_fcc_unit_len(rho, mass)
-    print("lattice param:", L)
-    T = rdf_data_dict[sys_params['data']]['T']
+    data_str_list = sys_params['data']
 
-    # initialize states with ASE 
-    atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                              symbol='H',
-                              size=(size, size, size),
-                              latticeconstant= L,
-                              pbc=True)
-    system = System(atoms, device=device)
-    system.set_temperature(T)
+    system_list = []
+    for data_str in data_str_list:
+        system = get_system(data_str, device, size) 
+        if sys_params['anneal_flag'] == 'True':
+            system.set_temperature(assignments['start_T'] * units.kB)
+        system_list.append(system)
 
-    print(system.get_temperature())
+    # Initialize potentials, one model that simulate all 
+    schnet = get_model(gnn_params)
 
-    # Initialize potentials 
-    model = get_model(gnn_params)
-    GNN = GNNPotentials(system, model, cutoff=cutoff)
-    pair = PairPotentials(system, ExcludedVolume, lj_params,
-                    cutoff=8.0,
-                    ).to(device)
+    # build GNN_list 
+    model_list = []
+    for i, data_str in enumerate(data_str_list):
+        GNN = GNNPotentials(system_list[i], schnet, cutoff=cutoff)
+        pair = PairPotentials(system_list[i], ExcludedVolume, lj_params,
+                        cutoff=8.0,
+                        ).to(device)
 
-    model = Stack({'gnn': GNN, 'pair': pair})
+        model = Stack({'gnn': GNN, 'pair': pair})
+        model_list.append(model)
 
-    # define the equation of motion to propagate 
-    diffeq = NoseHooverChain(model, 
-            system,
-            Q=50.0, 
-            T=T * units.kB,
-            num_chains=5, 
-            adjoint=True).to(device)
+    sim_list = [get_sim(system_list[i], model_list[i], data_str)  for i, data_str in enumerate(data_str_list)]
 
-    # define simulator with 
-    sim = Simulations(system, diffeq)
-    start = rdf_data_dict[sys_params['data']]['start']
-    end = rdf_data_dict[sys_params['data']]['end']
+    g_obs_list = []
+    obs_list = []
+    bins_list = []
 
-    # initialize observable function 
-    obs = rdf(system, nbins, (start, end) )
-    vacf_obs = vacf(system, t_range=int(tau//2))
+    for i, data_str in enumerate(data_str_list):
+        x, g_obs, obs = get_observer(system_list[i], data_str, nbins)
+        bins_list.append(x)
+        g_obs_list.append(g_obs)
+        obs_list.append(obs)
 
-    # angle observation function 
-    # angle_start = 0.45
-    # angle_end = 3.1
-    # nbins_angle_test = 64
-
-    # angle_start_train = assignments['angle_start_train']
-
-    # A function to generate list of data nd functions 
-    # angle_obs_train_list 
-    # angle_obs_test_list 
-    # angle_exp_train 
-    # angle_exp_list 
-
-    # nbins_angle_train = assignments['nbins_angle_train']
-    # angle_obs_train = angle_distribution(system, nbins_angle_train, (angle_start_train, angle_end), cutoff=assignments['angle_cutoff']) # 3.25 is from the PCCP paper
-    # angle_obs_test = angle_distribution(system, nbins_angle_test, (angle_start, angle_end), cutoff=assignments['angle_cutoff']) 
-
-    # vacf_obs = vacf(system, t_range=tau)
-
-    # # get experimental angle distribution 
-    # angle_exp_train = exp_angle_data(nbins_angle_train, (angle_start_train, angle_end), fn='../data/water_angle_pccp.csv')
-    # angle_exp_train = torch.Tensor(angle_exp_train).to(device)
-    # angle_exp_test = exp_angle_data(nbins_angle_test, (angle_start, angle_end), fn='../data/water_angle_pccp.csv')
-    # angle_exp_test = torch.Tensor(angle_exp_test).to(device)
-
-    # ANGLE_FACTOR = (angle_end - angle_start)/nbins_angle_train
-
-    #angle_exp_train = angle_exp_train / ANGLE_FACTOR
-
-    xnew = np.linspace(start, end, nbins)
-    # get experimental rdf 
-    count_obs, g_obs = get_exp_rdf(data, nbins, (start, end), obs)
     # define optimizer 
-    optimizer = torch.optim.SGD(list(diffeq.parameters() ), lr=assignments['lr'])
+    optimizer = torch.optim.Adam(list(schnet.parameters() ), lr=assignments['lr'])
 
     loss_log = []
     test_loss_log = []
@@ -252,40 +298,55 @@ def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 
                                                   'min', 
                                                   min_lr=5.0e-8, 
-                                                  verbose=True, factor = 0.5, patience= 30,
+                                                  verbose=True, factor = 0.5, patience= 40,
                                                   threshold=5e-5)
 
     for i in range(0, n_epochs):
         
-        current_time = datetime.now() 
-        trajs = sim.simulate(steps=tau, frequency=int(tau))
-        v_t, q_t, pv_t = trajs 
+        loss_js = torch.Tensor([0.0]).to(device)
+        loss_mse = torch.Tensor([0.0]).to(device)
 
+        # temperature annealing 
 
-        if torch.isnan(q_t.reshape(-1)).sum().item() > 0:
-            return 5 - (i / n_epochs) * 5
+        for j, sim in enumerate(sim_list):
 
-        # if i >= assignments['angle_train_start']:
+            if sys_params['anneal_flag'] == 'True':
 
-        #     bins, sim_angle_density, cos = angle_obs_train(q_t[::frameskip])
+                if i % assignments['anneal_freq'] == 0:
+                    def get_temp(T_start, T_equil, n_epochs, i, anneal_rate):
+                        return (T_start - T_equil) * np.exp( - i * (1/n_epochs) * anneal_rate) + T_equil
 
-        #     sim_angle_density =  sim_angle_density / ANGLE_FACTOR
+                    anneal_rate = assignments['anneal_rate']
 
-        #     loss_angle = JS_rdf(sim_angle_density, angle_exp_train)  * assignments['angle_JS_weight'] + \
-        #                  (sim_angle_density - angle_exp_train).pow(2).mean() * assignments['angle_MSE_weight']
+                    T_equil = rdf_data_dict[data_str_list[j]]['T']
+                    T_start = assignments['start_T']
+                    new_T  = get_temp(T_start, T_equil, n_epochs, i, anneal_rate)
+                    sim.intergrator.update_T(new_T * units.kB)
 
-        # else:
-        #     loss_angle = torch.Tensor([0.0]).to(device)
+                    print("update T:", new_T)
 
-        _, bins, g = obs(q_t[::frameskip])
-        
-        # this shoud be wrapped in some way 
-        loss_js = JS_rdf(g_obs, g)
-        loss = loss_js + assignments['mse_weight'] * (g- g_obs).pow(2).mean() # + loss_angle
+            current_time = datetime.now() 
+            trajs = sim.simulate(steps=tau, frequency=int(tau))
+            v_t, q_t, pv_t = trajs 
 
-        print(loss_js.item(),  loss.item())
+            if torch.isnan(q_t.reshape(-1)).sum().item() > 0:
+                return 5 - (i / n_epochs) * 5
+
+            _, bins, g = obs_list[j](q_t[::frameskip])
+            
+            # this shoud be wrapped in some way 
+            loss_js += JS_rdf(g_obs_list[j], g)
+            loss_mse += assignments['mse_weight'] * (g- g_obs_list[j]).pow(2).mean() 
+
+            if i % 20 == 0:
+                plot_rdfs(bins_list[j], g_obs_list[j], g, "{}_{}".format(data_str_list[j], i),
+                             model_path, pname=i)
+
+        loss = loss_js + loss_mse 
         loss.backward()
         
+        print(loss_js.item(),  loss.item())
+
         duration = (datetime.now() - current_time)
         
         optimizer.step()
@@ -293,33 +354,12 @@ def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
 
         scheduler.step(loss)
 
-        if i % 20 == 0:
-            plot_rdfs(xnew, g_obs, g, i, model_path)
-
-            # # plotting angles 
-            # def plot_angle(sim_angle, exp_angle, angle_start, angle_angle, fname, path, nbins_angle):
-            #     bins = np.linspace(angle_start, angle_end, nbins_angle)
-            #     plt.plot(bins * 180/np.pi, sim_angle.detach().cpu(), linewidth=4, alpha=0.6, label='sim.' )
-            #     plt.plot(bins * 180/np.pi, exp_angle.detach().cpu(), linewidth=2,linestyle='--', c='black', label='exp.')
-            #     plt.show()
-            #     plt.savefig(path + '/angle_{}.jpg'.format(fname), bbox_inches='tight')
-            #     plt.close()
-            # # measure angle distirbutions 
-            # bins, sim_angle_density, angle_sim = angle_obs_test(q_t[::5].detach())
-            # test_angle_loss = JS_rdf(sim_angle_density, angle_exp_test)
-
-            # # This is computed incorrectly, the loss_js should be computed on a test calculations. 
-            # test_loss_log.append(test_angle_loss.item() + loss_js.item())
-            # plot_angle(sim_angle_density, angle_exp_test, angle_start, angle_end, i, path=model_path, nbins_angle=nbins_angle_test)
-
-            test_loss_log.append(loss_js.item())
-
-            # plot VACF 
-            vacf_sim = vacf_obs(v_t.detach())
-            plt.plot(vacf_sim.detach().cpu().numpy())
-            plt.savefig(model_path + '/{}_{}.jpg'.format("vacf", i), bbox_inches='tight')
-            plt.show()
-            plt.close()
+            # # plot VACF 
+            # vacf_sim = vacf_obs(v_t.detach())
+            # plt.plot(vacf_sim.detach().cpu().numpy())
+            # plt.savefig(model_path + '/{}_{}.jpg'.format("vacf", i), bbox_inches='tight')
+            # plt.show()
+            # plt.close()
 
         if torch.isnan(loss):
             plt.plot(loss_log)
@@ -327,7 +367,7 @@ def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
             plt.savefig(model_path + '/loss.jpg')
             plt.close()
             if len(test_loss_log) != 0:   
-                return np.array(test_loss_log[-5:-1]).mean() + 1.0
+                return np.array(loss_log[-5:-1]).mean() + 1.0
             else:
                 return 2 - (i / n_epochs) * 2
 
@@ -352,36 +392,44 @@ def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
     plt.savefig(model_path + '/loss.jpg', bbox_inches='tight')
     plt.close()
 
-    train_traj = [var[1] for var in diffeq.traj]
-    save_traj(system, train_traj, model_path + '/train.xyz', skip=10)
 
-    # Inference 
-    sim_trajs = []
-    for i in range(n_sim):
-        _, q_t, _ = sim.simulate(steps=100, frequency=25)
+    for j, sim in enumerate(sim_list):    
+        train_traj = [var[1] for var in sim.intergrator.traj]
+        save_traj(system_list[j], train_traj, model_path + '/{}_train.xyz'.format(data_str_list[j]), skip=10)
 
-        if torch.isnan(q_t.reshape(-1)).sum().item() > 0:
-            return 5.0
+    total_loss = 0.0
 
-        sim_trajs.append(q_t[-1].detach().cpu().numpy())
+    for j, sim in enumerate(sim_list):
+        # Inference 
+        sim_trajs = []
 
-    sim_trajs = torch.Tensor(np.array(sim_trajs)).to(device)
-    sim_trajs.requires_grad = False # no gradient required 
+        for i in range(n_sim):
+            _, q_t, _ = sim.simulate(steps=100, frequency=25)
 
-    # compute equilibrate rdf with finer bins 
-    test_nbins = 128
-    obs = rdf(system, test_nbins,  (start, end))
-    xnew = np.linspace(start, end, test_nbins)
-    count_obs, g_obs = get_exp_rdf(data, test_nbins, (start, end), obs) # recompute exp. rdf
-    _, bins, g = obs(sim_trajs[::5]) # compute simulated rdf
+            if torch.isnan(q_t.reshape(-1)).sum().item() > 0:
+                return 5.0
 
-    # compute equilibrated rdf 
-    loss_js = JS_rdf(g_obs, g)
+            sim_trajs.append(q_t[-1].detach().cpu().numpy())
 
-    save_traj(system, sim_trajs.detach().cpu().numpy(),  
-        model_path + '/sim.xyz', skip=1)
+        sim_trajs = torch.Tensor(np.array(sim_trajs)).to(device)
+        sim_trajs.requires_grad = False # no gradient required 
 
-    plot_rdfs(xnew, g_obs, g, "final", model_path)
+        # compute equilibrate rdf with finer bins 
+        test_nbins = 128
+
+        x, g_obs, obs = get_observer(system_list[j], data_str_list[j], test_nbins)
+
+        _, bins, g = obs(sim_trajs[::5]) # compute simulated rdf
+
+        # compute equilibrated rdf 
+        loss_js = JS_rdf(g_obs, g)
+
+        save_traj(system_list[j], sim_trajs.detach().cpu().numpy(),  
+            model_path + '/{}_sim.xyz'.format(data_str_list[j]), skip=1)
+
+        plot_rdfs(x, g_obs, g, "{}_final".format(data_str_list[j]), model_path, pname='final')
+
+        total_loss += loss_js.item()
 
     # ANGLE_FACTOR = (angle_end - angle_start)/nbins_angle_test
 
