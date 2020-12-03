634dc15818c8e8fa560b16cea3d7384264c88af5
wwj <wwj@mit.edu>
2020-12-03 00:33:16 -0500

    swap some order, but why?

--- torchmd/tinydiffeq.py
+++ torchmd/tinydiffeq.py
@@ -9,55 +9,6 @@ import abc
     [1] Ricky T. Q. Chen, Yulia Rubanova, Jesse Bettencourt, David Duvenaud. "Neural Ordinary Differential Equations." Advances in Neural Information Processing Systems. 2018. 
 '''
 
-# some utils 
-def _flatten(sequence):
-    flat = [p.contiguous().view(-1) for p in sequence]
-    return torch.cat(flat) if len(flat) > 0 else torch.tensor([])
-
-
-def _flatten_convert_none_to_zeros(sequence, like_sequence):
-    flat = [
-        p.contiguous().view(-1) if p is not None else torch.zeros_like(q).view(-1)
-        for p, q in zip(sequence, like_sequence)
-    ]
-    return torch.cat(flat) if len(flat) > 0 else torch.tensor([])
-
-def _decreasing(t):
-    return (t[1:] < t[:-1]).all()
-
-def _check_inputs(func, y0, t):
-    tensor_input = False
-    if torch.is_tensor(y0):
-        tensor_input = True
-        y0 = (y0,)
-        _base_nontuple_func_ = func
-        func = lambda t, y: (_base_nontuple_func_(t, y[0]),)
-    assert isinstance(y0, tuple), 'y0 must be either a torch.Tensor or a tuple'
-    for y0_ in y0:
-        assert torch.is_tensor(y0_), 'each element must be a torch.Tensor but received {}'.format(type(y0_))
-
-    if _decreasing(t):
-        t = -t
-        _base_reverse_func = func
-        func = lambda t, y: tuple(-f_ for f_ in _base_reverse_func(-t, y))
-
-    for y0_ in y0:
-        if not torch.is_floating_point(y0_):
-            raise TypeError('`y0` must be a floating point Tensor but is a {}'.format(y0_.type()))
-    if not torch.is_floating_point(t):
-        raise TypeError('`t` must be a floating point Tensor but is a {}'.format(t.type()))
-
-    return tensor_input, func, y0, t
-
-def _handle_unused_kwargs(solver, unused_kwargs):
-    if len(unused_kwargs) > 0:
-        warnings.warn('{}: Unexpected arguments {}'.format(solver.__class__.__name__, unused_kwargs))
-
-def _assert_increasing(t):
-	assert (t[1:] > t[:-1]).all(), 't must be strictly increasing or decrasing'
-
-
-
 # Solver Class 
 class FixedGridODESolver(object):
     __metaclass__ = abc.ABCMeta
@@ -150,3 +101,51 @@ def rk4_alt_step_func(func, t, dt, y, k1=None):
     k3 = func(t + dt * 2 / 3, tuple(y_ + dt * (k1_ / -3 + k2_) for y_, k1_, k2_ in zip(y, k1, k2)))
     k4 = func(t + dt, tuple(y_ + dt * (k1_ - k2_ + k3_) for y_, k1_, k2_, k3_ in zip(y, k1, k2, k3)))
     return tuple((k1_ + 3 * k2_ + 3 * k3_ + k4_) * (dt / 8) for k1_, k2_, k3_, k4_ in zip(k1, k2, k3, k4))
+
+
+def _flatten(sequence):
+    flat = [p.contiguous().view(-1) for p in sequence]
+    return torch.cat(flat) if len(flat) > 0 else torch.tensor([])
+
+
+def _flatten_convert_none_to_zeros(sequence, like_sequence):
+    flat = [
+        p.contiguous().view(-1) if p is not None else torch.zeros_like(q).view(-1)
+        for p, q in zip(sequence, like_sequence)
+    ]
+    return torch.cat(flat) if len(flat) > 0 else torch.tensor([])
+
+def _decreasing(t):
+    return (t[1:] < t[:-1]).all()
+
+def _check_inputs(func, y0, t):
+    tensor_input = False
+    if torch.is_tensor(y0):
+        tensor_input = True
+        y0 = (y0,)
+        _base_nontuple_func_ = func
+        func = lambda t, y: (_base_nontuple_func_(t, y[0]),)
+    assert isinstance(y0, tuple), 'y0 must be either a torch.Tensor or a tuple'
+    for y0_ in y0:
+        assert torch.is_tensor(y0_), 'each element must be a torch.Tensor but received {}'.format(type(y0_))
+
+    if _decreasing(t):
+        t = -t
+        _base_reverse_func = func
+        func = lambda t, y: tuple(-f_ for f_ in _base_reverse_func(-t, y))
+
+    for y0_ in y0:
+        if not torch.is_floating_point(y0_):
+            raise TypeError('`y0` must be a floating point Tensor but is a {}'.format(y0_.type()))
+    if not torch.is_floating_point(t):
+        raise TypeError('`t` must be a floating point Tensor but is a {}'.format(t.type()))
+
+    return tensor_input, func, y0, t
+
+def _handle_unused_kwargs(solver, unused_kwargs):
+    if len(unused_kwargs) > 0:
+        warnings.warn('{}: Unexpected arguments {}'.format(solver.__class__.__name__, unused_kwargs))
+
+def _assert_increasing(t):
+    assert (t[1:] > t[:-1]).all(), 't must be strictly increasing or decrasing'
+
