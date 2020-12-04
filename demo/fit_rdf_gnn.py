42e5479cf2b776af60ef9ca20c266f35e8a52a0f
wwj <wwj@mit.edu>
2020-12-04 01:35:06 -0500

    demos

---
+++ demo/fit_rdf_gnn.py
@@ -0,0 +1,339 @@
+import torchmd
+from nff.train import get_model
+from torchmd.system import System
+from torchmd.interface import GNNPotentials, PairPotentials, Stack
+from torchmd.md import Simulations
+from torchmd.observable import angle_distribution
+from torchmd.potentials import pairMLP
+from ase import units
+from data import exp_rdf_data_dict, get_exp_rdf, get_unit_len
+from plot import *
+from scripts import * 
+import json
+
+import math 
+
+width_dict = {'tiny': 64,
+               'low': 128,
+               'mid': 256, 
+               'high': 512}
+
+gaussian_dict = {'tiny': 16,
+               'low': 32,
+               'mid': 64, 
+               'high': 128}
+
+
+def save_traj(system, traj, fname, skip=10):
+    atoms_list = []
+    for i, frame in enumerate(traj):
+        if i % skip == 0: 
+            frame = Atoms(positions=frame, numbers=system.get_atomic_numbers())
+            atoms_list.append(frame)
+    ase.io.write(fname, atoms_list) 
+
+def JS_rdf(g_obs, g):
+    e0 = 1e-4
+    g_m = 0.5 * (g_obs + g)
+    loss_js =  ( -(g_obs + e0 ) * (torch.log(g_m + e0 ) - torch.log(g_obs +  e0)) ).mean()
+    loss_js += ( -(g + e0 ) * (torch.log(g_m + e0 ) - torch.log(g + e0) ) ).mean()
+
+    return loss_js
+
+def plot_rdfs(bins, target_g, simulated_g, fname, path, pname=None):
+    plt.title("epoch {}".format(pname))
+    plt.plot(bins, simulated_g.detach().cpu().numpy() , linewidth=4, alpha=0.6, label='sim.' )
+    plt.plot(bins, target_g.detach().cpu().numpy(), linewidth=2,linestyle='--', c='black', label='exp.')
+    plt.xlabel("$\AA$")
+    plt.ylabel("g(r)")
+    plt.savefig(path + '/{}.jpg'.format(fname), bbox_inches='tight')
+    plt.show()
+    plt.close()
+
+
+def get_system(data_tag, device, size):
+
+    rho = exp_rdf_data_dict[data_tag]['rho']
+    mass = exp_rdf_data_dict[data_tag]['mass']
+    T = exp_rdf_data_dict[data_tag]['T']
+
+    # initialize states with ASE 
+    cell_module = exp_rdf_data_dict[data_tag]['cell']
+    N_unitcell = exp_rdf_data_dict[data_tag]['N_unitcell']
+
+    L = get_unit_len(rho, mass, N_unitcell)
+
+    print("lattice param: {:.3f} Angstroms".format(L))
+
+    atoms = cell_module(symbol=exp_rdf_data_dict[data_tag]['element'],
+                          size=(size, size, size),
+                          latticeconstant= L,
+                          pbc=True)
+    system = System(atoms, device=device)
+    system.set_temperature(T * ase.units.kB)
+
+    return system 
+
+def get_sim(system, model, data_tag):
+
+    T = exp_rdf_data_dict[data_tag]['T']
+
+    diffeq = NoseHooverChain(model, 
+            system,
+            Q=50.0, 
+            T=T * units.kB,
+            num_chains=5, 
+            adjoint=True).to(system.device)
+
+    # define simulator with 
+    sim = Simulations(system, diffeq)
+
+    return sim
+
+def get_observer(system, data_tag, nbins):
+
+    data_path = exp_rdf_data_dict[data_tag]['fn']
+    data = np.loadtxt(data_path, delimiter=',')
+
+    # define the equation of motion to propagate 
+    start = exp_rdf_data_dict[data_tag]['start']
+    end = exp_rdf_data_dict[data_tag]['end']
+
+    xnew = np.linspace(start, end, nbins)
+    # initialize observable function 
+    obs = rdf(system, nbins, (start, end) )
+
+    # get experimental rdf 
+    count_obs, g_obs = get_exp_rdf(data, nbins, (start, end), obs)
+
+    return xnew, g_obs, obs
+
+def get_temp(T_start, T_equil, n_epochs, i, anneal_rate):
+    return (T_start - T_equil) * np.exp( - i * (1/n_epochs) * anneal_rate) + T_equil
+
+
+def get_gnn_potential(assignments,  sys_params):
+    lj_params = {'epsilon': assignments['epsilon'], 
+                 'sigma': assignments['sigma'], 
+                 'power': 12}
+
+    gnn_params = {
+        'n_atom_basis': width_dict[assignments['n_atom_basis']],
+        'n_filters': width_dict[assignments['n_filters']],
+        'n_gaussians': int(assignments['cutoff']//assignments['gaussian_width']),
+        'n_convolutions': assignments['n_convolutions'],
+        'cutoff': assignments['cutoff'],
+        'trainable_gauss': False
+    }
+
+    net = get_model(gnn_params)
+    prior = ExcludedVolume(**lj_params)
+    return net, prior
+
+def get_pair_potential(assignments, sys_params):
+
+    mlp_parmas = {'n_gauss': int(cutoff//assignments['gaussian_width']), 
+              'r_start': 0.0,
+              'r_end': cutoff, 
+              'n_width': assignments['n_width'],
+              'n_layers': assignments['n_layers'],
+              'nonlinear': assignments['nonlinear']}
+
+    lj_params = {'epsilon': assignments['epsilon'], 
+         'sigma': assignments['sigma'],
+        "power": assignments['power']}
+
+    net = pairMLP(**mlp_parmas)
+    prior = ExcludedVolume(**lj_params)
+
+    return net, prior
+
+def build_simulators(data_list, system_list, net, prior, cutoff, pair_flag): 
+    model_list = []
+    for i, data_tag in enumerate(data_list):
+        prior = PairPotentials(system_list[i], prior,
+                        cutoff=cutoff,
+                        ).to(system_list[i].device)
+
+        if pair_flag:
+            NN = PairPotentials(system_list[i], net,
+                cutoff=cutoff,
+                ).to(device)
+        else:
+            NN = GNNPotentials(system_list[i], 
+                            net, 
+                            cutoff=cutoff)
+
+        model = Stack({'nn': NN, 'pair': prior})
+        model_list.append(model)
+
+    sim_list = [get_sim(system_list[i], 
+                        model_list[i], 
+                        data_tag) for i, data_tag in enumerate(data_list)]
+
+    return sim_list
+
+
+def fit_rdf(assignments, i, suggestion_id, device, sys_params, project_name):
+    # parse params 
+    size = sys_params['size']
+    dt = sys_params['dt']
+    n_epochs = sys_params['n_epochs'] 
+    n_sim = sys_params['n_sim'] 
+    cutoff = assignments['cutoff']
+    nbins = assignments['nbins']
+    tau = assignments['opt_freq'] 
+
+
+    print(json.dumps( assignments, indent=1) )
+
+    model_path = '{}/{}'.format(project_name, suggestion_id)
+    os.makedirs(model_path)
+
+    print("Training for {} epochs".format(n_epochs))
+
+    # get cell parameter and data 
+    train_list = sys_params['data']
+
+    if sys_params['val']:
+        all_sys = train_list + val_str_list
+    else:
+        all_sys = train_list
+
+    system_list = []
+    for data_tag in all_sys:
+        system = get_system(data_tag, device, size) 
+        if sys_params['anneal_flag'] == 'True':
+            system.set_temperature(assignments['start_T'] * units.kB)
+        system_list.append(system)
+
+    # Initialize potentials, one model that simulate all 
+    if sys_params["pair_flag"]:
+        net, prior = get_pair_potential(assignments, sys_params)
+
+    else:
+        net, prior = get_gnn_potential(assignments, sys_params)
+
+    sim_list = build_simulators(all_sys, system_list, net, prior, 
+                                cutoff=cutoff, pair_flag=sys_params["pair_flag"])
+
+    g_target_list = []
+    obs_list = []
+    bins_list = []
+
+    for i, data_tag in enumerate(all_sys):
+        x, g_obs, obs = get_observer(system_list[i], data_tag, nbins)
+        bins_list.append(x)
+        g_target_list.append(g_obs)
+        obs_list.append(obs)
+
+    # define optimizer 
+    optimizer = torch.optim.Adam(list(net.parameters()), lr=assignments['lr'])
+
+    loss_log = []
+
+    solver_method = 'NH_verlet'
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 
+                                                  'min', 
+                                                  min_lr=0.9e-7, 
+                                                  verbose=True, factor = 0.5, patience=15,
+                                                  threshold=5e-5)
+
+    for i in range(0, n_epochs):
+
+        loss_js = torch.Tensor([0.0]).to(device)
+        loss_mse = torch.Tensor([0.0]).to(device)
+
+        # temperature annealing 
+        for j, sim in enumerate(sim_list):
+            data_tag = all_sys[j]
+
+            if sys_params['anneal_flag'] == 'True' and i % assignments['anneal_freq'] == 0:
+
+                T_equil = exp_rdf_data_dict[data_tag]['T']
+                T_start = assignments['start_T']
+                new_T  = get_temp(T_start, T_equil, n_epochs, i, assignments['anneal_rate'])
+                sim.integrator.update_T(new_T * units.kB)
+
+                print("update T: {:.2f}".format(new_T))
+
+            v_t, q_t, pv_t = sim.simulate(steps=tau, frequency=int(tau))
+
+            _, bins, g = obs_list[j](q_t[::5])
+            
+            # only optimize on data that needs training 
+            if data_tag in train_list:
+                loss_js += JS_rdf(g_target_list[j], g)
+                loss_mse += assignments['mse_weight'] * (g- g_target_list[j]).pow(2).mean() 
+
+            if i % 20 == 0:
+                plot_rdfs(bins_list[j], g_target_list[j], g, "{}_{}".format(data_tag, i),
+                             model_path, pname=i)
+
+                if sys_params['pair_flag']:
+                    potential = plot_pair( path=model_path,
+                                 fn=str(i),
+                                  model=net, 
+                                  prior=pair, 
+                                  device=device)
+
+        loss = loss_js + loss_mse 
+        loss.backward()
+        
+        print("epoch {} | loss: {:.5f}".format(i, loss.item()) ) 
+        
+        optimizer.step()
+        optimizer.zero_grad()
+        scheduler.step(loss)
+
+        loss_log.append(loss_js.item() )
+
+        if optimizer.param_groups[0]["lr"] <= 1.0e-7:
+            print("training converged")
+            break
+
+    plt.plot(loss_log)
+    plt.savefig(model_path + '/loss.jpg', bbox_inches='tight')
+    plt.close()
+
+    total_loss = 0.0
+    for j, sim in enumerate(sim_list):    
+
+        data_tag = all_sys[j]
+
+        train_traj = sim.log['positions']
+
+        if all_sys[j] in train_list:
+            save_traj(system_list[j], train_traj, model_path + '/{}_train.xyz'.format(data_tag), skip=10)
+        else:
+            save_traj(system_list[j], train_traj, model_path + '/{}_val.xyz'.format(data_tag), skip=10)
+
+        # Inference 
+        sim_trajs = []
+
+        for i in range(n_sim):
+            _, q_t, _ = sim.simulate(steps=100, frequency=25)
+
+        sim_trajs = torch.Tensor(np.array(sim_trajs)).to(device)
+        sim_trajs = torch.Tensor( np.stack( sim.log['positions'])).to(system.device)
+
+        # compute equilibrate rdf with finer bins 
+        test_nbins = 128
+
+        x, g_obs, obs = get_observer(system_list[j], data_tag, test_nbins)
+
+        _, bins, g = obs(sim_trajs[::5]) # compute simulated rdf
+
+        # compute equilibrated rdf 
+        loss_js = JS_rdf(g_obs, g)
+
+        save_traj(system_list[j], sim_trajs.detach().cpu().numpy(),  
+            model_path + '/{}_sim.xyz'.format(data_tag), skip=1)
+
+        plot_rdfs(x, g_obs, g, "{}_final".format(data_tag), model_path, pname='final')
+
+        total_loss += loss_js.item()
+
+    np.savetxt(model_path + '/loss.csv', np.array(loss_log))
+
+    return loss_js.item() 
---
+++ demo/isom.py
@@ -0,0 +1,233 @@
+"""Optimize the quantum yield of a retinal model (Hahn, Susanne, and Gerhard Stock.
+"Quantum-mechanical modeling of the femtosecond isomerization in rhodopsin."
+The Journal of Physical Chemistry B 104.6 (2000): 1146-1149.)"""
+
+
+import torch
+import numpy as np
+import os
+import torch
+import random
+import pdb
+from math import pi
+import sys
+import json
+
+from torchmd.md import Isomerization
+from torchmd.sovlers import odeint_adjoint as odeint
+
+# time conversion
+FS_TO_EV = 41.341 / 27.2 
+# time step
+DT = 2 * pi / 2.8 / 30
+# max time
+TMAX = 1500 * FS_TO_EV
+# TMAX = 1 * FS_TO_EV
+
+NUM_EPOCHS = 50
+# NUM_EPOCHS = 5
+
+
+# pulse duration
+TAU =  10 * FS_TO_EV 
+# center pulse frequency
+W0 = 2.4
+# pulse time
+TP = 3 * TAU
+DEVICE = 2
+
+# files for saving
+YIELD_FILE = 'isom_results/q_yields.json'
+FIELD_FILE = 'isom_results/e_fields.json'
+T_YIELD_FILE = 'isom_results/t_dep_yields.json'
+
+def make_quants():
+
+
+    """Load matrices for the retinal model.
+    Args:
+        None
+    Returns:
+        dic (dict): dictionary of matrices (operators)
+    """
+
+    # hamiltonian
+    ham = torch.tensor(np.load('../data/isom/hamiltonian.npy')).float().to(DEVICE)
+    # dipole operator
+    dipole =  torch.tensor(np.load('../data/isom/unitless_mu.npy')).float().to(DEVICE)
+    # product projection operator
+    prod_op = torch.tensor(np.load('../data/isom/Pt_11.npy')).float().to(DEVICE)
+    # reactant projection operator
+    reac_op =  torch.tensor(np.load('../data/isom/Pc_00.npy')).float().to(DEVICE)
+    # initial wave function is in the ground state
+    # note that we double its size: the first half of the elements 
+    # are the real part, and the second half is the imaginary part
+    psi_0 = torch.zeros(int(2*len(ham))).to(DEVICE)
+    psi_0[0] = 1
+
+
+    dic = {"ham": ham, "dipole": dipole, "prod_op": prod_op,
+           "reac_op": reac_op, "psi_0": psi_0}
+
+    return dic
+
+
+def initialize_Et(dt=DT, tmax=TMAX, w0=W0, tau=TAU, tp=TP):
+
+    """Initialize a reasonable guess for the electric field
+    and create the time grid.
+    Args:
+        dt (float): time step
+        tmax (float): max time
+        w0 (float): pulse center frequency
+        tau (float): pulse duration
+        tp (float): pulse arrival time
+    Returns:
+        combined (torch.Tensor): a tensor with elements
+            (t, E(t))
+        t_grid (torch.Tensor): time grid for the simulation
+        t_grid_0 (torch.Tensor): coarse time grid for the
+            first half of the simulation for E(t)
+    """
+
+    # number of total steps
+    num_steps = int(tmax/dt)
+    # number of steps during which the electric field
+    # can be non-zero. Take larger time steps so that
+    # it can't vary more quickly than we can resolve
+    first_num_steps = int(tmax/dt/5)
+
+    # time grid for the electric field part
+    t_grid_0 = torch.linspace(0, tmax/2, first_num_steps)
+    # time grid for the rest
+    t_grid_1 = torch.linspace(t_grid_0[-1] + dt, tmax, int(num_steps/2))
+
+    # time grid for the rest of the simulation (more steps, finer grid)
+    t_grid = torch.linspace(0, tmax, num_steps)
+
+
+    # electric field amplitude: this number maximizes the population that
+    # will make it to the excited state
+    e0 = pi**0.5 / tau 
+    # E(t)
+    e_t = e0 * np.cos(w0 * (t_grid_0-tp)) * np.exp(-(t_grid_0-tp)**2 / tau**2)
+    # [t, E(t)]
+    combined = torch.stack((t_grid_0, e_t), dim=-1)
+
+    return combined, t_grid, t_grid_0
+
+def calc_yield(psi_t, prod_op, reac_op):
+    """ Calculate the quantum yield.
+    
+    Args:
+        psi_t (torch.Tensor): wave function as a function of time
+        prod_op (torch.Tensor): product operator
+        reac_op (torch.Tensor): reactant operator
+    Returns:
+        expec_t (list): quantum yield as a function of time
+    
+    """
+
+    # dimension of the Hilbert space
+    dim = int(len(psi_t[-1])/2)
+    expec_t = []
+
+    # loop over times
+    for i in range(len(psi_t)):
+
+        # real and imaginary parts of psi
+        psi_r = psi_t[i, :dim]
+        psi_i = psi_t[i, dim:]
+
+        # expression for expectation values from the real and imaginary parts.
+        # Valid for real-valued operators (which is the case here)
+
+        # <product>
+        expec_r = (psi_r * (torch.matmul(prod_op, psi_r))).sum().reshape(-1)
+        expec_i = (psi_i * (torch.matmul(prod_op, psi_i))).sum().reshape(-1)
+
+        # <reactant>
+        expec_rC = (psi_r * (torch.matmul(reac_op, psi_r))).sum().reshape(-1)
+        expec_iC = (psi_i * (torch.matmul(reac_op, psi_i))).sum().reshape(-1)
+
+        # subtract the part that remained in the ground state
+        pg = psi_r[0]**2 + psi_i[0] **2
+        y = (expec_r + expec_i) / ((expec_r + expec_i) + (expec_rC + expec_iC) - pg)
+
+        expec_t.append(y)
+
+    return expec_t
+
+
+def objective(expec_t, look_back=20000):
+
+    """
+    Creates the objective function to minimize.
+    Args:
+        expec_t (list): time-dependent quantum yield
+        look_back (int): number of previous time steps
+            over which to average the yield
+    Returns:
+        obj (torch.Tensor): objective function
+    Note:
+        20,000 time steps = 1 ps, since dt = 0.05 fs,
+        so this defaults to averaging the QY over 1 ps.
+    """
+
+    # want to maximize quantum yield (i.e. minimize its negative)
+    obj =  -torch.mean(torch.cat(expec_t)[-look_back:])
+
+    return obj
+
+def main():
+
+    quant_dic = make_quants()
+    e_field, t, t_grid_et = initialize_Et()
+    max_e_t = max(t_grid_et)
+
+    # initialize the ode
+    ode = Isomerization(dipole=quant_dic["dipole"], e_field=e_field, ham=quant_dic["ham"],
+                    max_e_t=max_e_t, device=DEVICE).to(DEVICE)
+
+    # define optimizer 
+    trainable_params = filter(lambda p: p.requires_grad, ode.parameters())
+    optimizer = torch.optim.Adam(trainable_params, lr=1e-2)
+
+    q_yields = []
+    e_fields = []
+    t_yields = []
+
+    for i in range(NUM_EPOCHS):
+
+        print("simulation epoch {}".format(i))
+        psi_0 = quant_dic['psi_0'].to(DEVICE)
+        psi_t = odeint(ode, psi_0, t, method='rk4')
+        q_yield = calc_yield(psi_t, quant_dic["prod_op"].to(DEVICE), quant_dic["reac_op"].to(DEVICE))
+
+        loss = objective(q_yield)
+
+        loss.backward()
+        print("Average quantum yield is {}".format(-loss.item()))
+
+        q_yields.append(-loss.item())
+        e_fields.append(ode.e_field.cpu().detach().numpy().tolist())
+        t_dep_yield = [item.item() for item in q_yield]
+        t_yields.append(t_dep_yield)
+
+        with open(YIELD_FILE, 'w') as f:
+            json.dump(q_yields, f)
+        with open(T_YIELD_FILE, 'w') as f:
+            json.dump(t_yields, f)
+        with open(FIELD_FILE, 'w') as f:
+            json.dump(e_fields, f)
+
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+if __name__ == "__main__":
+
+    if os.path.exists("isom_results/") == False:
+        os.makedirs("isom_results/")
+
+    main()
