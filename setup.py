d98a0e742c41275e99e49ae9adca019034bed6ad
wwang2 <wwj@mit.edu>
2022-08-30 18:06:19 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
-import setuptools
 
+from setuptools import setup
 
-setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description='',
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="."),
-    python_requires=">=3.6",
+setup(
+   name='mdgrad',
+   version='0.1',
+   description='',
+   author='Wujie Wang',
+   author_email='wwj@mit.edu',
+   packages=['torchmd'], 
+   #install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
 )
