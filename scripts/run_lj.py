4d07f8fc79d7aef45c7e15bda8f3b0641afefb51
wwang2 <wwj@mit.edu>
2022-09-04 17:22:01 -0400

    try linting run_lj.py

--- scripts/run_lj.py
+++ scripts/run_lj.py
@@ -2,7 +2,9 @@ import argparse
 from fit_rdf_pair import *
 from datetime import datetime
 from datetime import date
-import random 
+import random
+import numpy as np
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-logdir", type=str)
@@ -11,7 +13,7 @@ parser.add_argument("-nruns", type=int, default=1)
 
 parser.add_argument("-paramset", type=str, default='None')
 
-# options to overide default param set 
+# options to overide default param set
 parser.add_argument("-sigma", type=float)
 parser.add_argument("-lr", type=float)
 parser.add_argument("-cutoff", type=float)
@@ -33,57 +35,57 @@ if params['dry_run']:
     n_sim = 2
 else:
     n_obs = 1000
-    n_epochs = 1000 # 1000
-    n_sim = 200 # 50 
+    n_epochs = 1000    # 1000
+    n_sim = 200   # 50
 
 logdir = params['logdir']
 
 
 if params['paramset'] != 'None':
-    import json 
+    import json
     assignments = json.load(open(params['paramset']))
 
 else:
-  # use default assignments 
-  assignments = {
-    "epsilon": 0.4,
-    "gaussian_width": 0.10,
-    "n_layers": 3,
-    "n_width": 128,
-    "nbins": 100,
-    "nonlinear": "ELU",
-    "opt_freq": params['opt_freq'], # 60
-    "power": 10,
-    "rdf_weight": 0.95,
-    "train_vacf": "True",
-    "lr": 0.002,
-    "sigma": 0.9,
-    "vacf_weight": 0.41,
-    "cutoff": 2.5
-  }
+    # use default assignments
+    assignments = {
+        "epsilon": 0.4,
+        "gaussian_width": 0.10,
+        "n_layers": 3,
+        "n_width": 128,
+        "nbins": 100,
+        "nonlinear": "ELU",
+        "opt_freq": params['opt_freq'],   # 60
+        "power": 10,
+        "rdf_weight": 0.95,
+        "train_vacf": "True",
+        "lr": 0.002,
+        "sigma": 0.9,
+        "vacf_weight": 0.41,
+        "cutoff": 2.5
+        }
 
 sys_params = {
-'dt': params['dt'],
-'size': 4,
-'n_epochs': n_epochs,
-'n_sim': n_sim,
-'data': params['data'],
-'val': params['val'],
-'t_range': 50, # 50 
-'skip': 5,
-'cutoff': assignments['cutoff'],
-'nbr_list_device': 'cpu',
-'topology_update_freq': params['update_freq']
+    'dt': params['dt'],
+    'size': 4,
+    'n_epochs': n_epochs,
+    'n_sim': n_sim,
+    'data': params['data'],
+    'val': params['val'],
+    't_range': 50,   # 50
+    'skip': 5,
+    'cutoff': assignments['cutoff'],
+    'nbr_list_device': 'cpu',
+    'topology_update_freq': params['update_freq']
 }
 
 if assignments['train_vacf'] == 'False':
     assignments['vacf_weight'] = 0.0
 
-# param overwrite 
+# param overwrite
 if params['trainvacf']:
     assignments['train_vacf'] = 'True'
 
-if params['sigma'] is not None :
+if params['sigma'] is not None:
     print("chaging default sigma from {} to {}".format(assignments['sigma'], params['sigma']))
     assignments['sigma'] = params['sigma']
 
@@ -104,16 +106,16 @@ if params['device'] is None:
 
 print(assignments)
 
-
 for i in range(params['nruns']):
 
     now = datetime.now()
     dt_string = now.strftime("%m-%d-%H-%M-%S") + str(random.randint(0, 100))
 
-    value = fit_lj(assignments=assignments, 
-                            suggestion_id=params['name'] + dt_string, 
-                            device=params['device'],
-                            sys_params=sys_params,
-                            project_name=logdir)
+    value = fit_lj(assignments=assignments,
+                   suggestion_id=params['name'] + dt_string,
+                   device=params['device'],
+                   sys_params=sys_params,
+                   project_name=logdir)
 
+# show test error
 print(value)
